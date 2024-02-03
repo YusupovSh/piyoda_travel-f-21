fa61eae458bafacecb7d5999b2cad22abf53f70f
YusupovSh <152324597+YusupovSh@users.noreply.github.com>
2024-02-03 13:39:47 +0500

    add BlueBanner style

--- src/constants/telegram.jsx
+++ src/constants/telegram.jsx
@@ -33,13 +33,14 @@ import DateIcon from '../assets/icons/categorie/date-icon.svg'
 import FoodIcon from '../assets/icons/categorie/food-icon.svg'
 import PlaneIcon from '../assets/icons/categorie/plane-icon.svg'
 
-const TelegramBotForm = ({ categorie }) => {
+const TelegramBotForm = ({ modalInfo, modal ,setModal}) => {
 
   const [tekshiruv, setTekshiruv] = useState(false);
 
+
   const [formData, setFormData] = useState({
-    name: '',
-    // tel: '+998 ',
+    name: "",
+    tel: "+998 ",
   });
 
 
@@ -48,16 +49,22 @@ const TelegramBotForm = ({ categorie }) => {
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
+  const apiToken = '6363926670:AAGMCzumLRvYglsRFwlG3iwgEynQRpjqn0U';
+  const chatId = '1135324282';
+
+  const message = `
+      Yangi foydalanuvchi:
+      Shahahar: ${modalInfo.card_name}  
+      Bilet Narxi:${modalInfo.img_price}
+      Uchish joyi: ${modalInfo.card_flymesta}
+      Ovkatlanish:${modalInfo.card_food}
+      Name: ${formData.name}
+      Phone Number: ${formData.tel}
+    `;
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const apiToken = '6363926670:AAGMCzumLRvYglsRFwlG3iwgEynQRpjqn0U';
-    const chatId = '1135324282'; 
-    
-    const message = `
-      Yangi foydalanuvchi:  
-      Name: ${formData.name}
-    `;
 
     try {
       if (formData.name == '') {
@@ -70,7 +77,7 @@ const TelegramBotForm = ({ categorie }) => {
         });
         setTekshiruv(true);
         // Malumotlarni uzatganizdan keyin input ishidegi malumot ochib ketadi
-        setFormData({ name: '' });
+        setFormData({ name: "", tel: "+998" });
       }
 
 
@@ -79,61 +86,59 @@ const TelegramBotForm = ({ categorie }) => {
     }
   };
 
-  if (formData.name == ! '') {
+  if (formData.name == ! '' || formData.tel == ! '+998 ') {
     setTekshiruv(true);
   }
 
+  console.log(modalInfo.id);
 
   return (
-    <div className="categorie__modal-container">
-{
-    categorie.map((item)=>(
-    <div className="categorie__modal" key={item.id}>
+    <div className={modal ? "categorie__modal-container" : "none"}>
+      <div className="categorie__modal" >
         <div className="categorie__modal-body">
-            <div className="categorie__modal__toptitle">
+          <div className="categorie__modal__toptitle">
             <h3 className="categorie__modal__toptitle-title">Заказать тур</h3>
-            <CloseOutlined className='categorie__modal__toptitle-icon' />
+            <CloseOutlined className='categorie__modal__toptitle-icon' onClick={() => setModal(false)}/>
+          </div>
+          <form className='categorie__modal-form' onSubmit={handleSubmit} method='get'>
+            <hr />
+            <div className="categorie__modal__info">
+              <h3 className="categorie__modal__info-title">{modalInfo.card_name}</h3>
+              <p className="categorie__modal__info-price">{modalInfo.img_price}</p>
+              <div className="categorie__offer__card__wrapper">
+                <div className="categorie__offer__card__wrapper-wrap">
+                  <img src={DateIcon} alt="" />
+                  <p className="categorie__offer__card__wrapper-date">{modalInfo.card_data} -  {modalInfo.card_data_day}</p>
+                </div>
+                <div className="categorie__offer__card__wrapper-wrap">
+                  <img src={PlaneIcon} alt="" />
+                  <p className="categorie__offer__card__wrapper-fly">{modalInfo.card_flymesta}</p>
+                </div>
+                <div className="categorie__offer__card__wrapper-wrap">
+                  <img src={FoodIcon} alt="" />
+                  <p className="categorie__offer__card__wrapper-fly">{modalInfo.card_food}</p>
+                </div>
+              </div>
             </div>
-            <form className='categorie__modal-form' onSubmit={handleSubmit} method='get'>
-                <hr />
-                <div className="categorie__modal__info">
-                <h3 className="categorie__modal__info-title">{item.card_name}</h3>
-                <p className="categorie__modal__info-price">{item.img_price} {item.img_people}</p>
-                <div className="categorie__offer__card__wrapper">
-                    <div className="categorie__offer__card__wrapper-wrap">
-                        <img src={DateIcon} alt="" />
-                        <p className="categorie__offer__card__wrapper-date">{item.card_data} - {item.card_data_day} </p>
-                    </div>
-                    <div className="categorie__offer__card__wrapper-wrap">
-                        <img src={PlaneIcon} alt="" />
-                        <p className="categorie__offer__card__wrapper-fly">{item.card_flymesta} </p>
-                    </div>
-                    <div className="categorie__offer__card__wrapper-wrap">
-                        <img src={FoodIcon} alt="" />
-                        <p className="categorie__offer__card__wrapper-fly">{item.card_food}</p>
-                    </div>
-                </div>
-                </div>
-                <hr />
-                <div className="categorie__modal__form">
-                <div className="categorie__modal__form-wrap">
-                    <label htmlFor="name" value={formData.name} onChange={handleChange}>Ваше имя</label>
-                    <input className='input-name' type="text" name='name' />
-                </div>
-                {/* <div className="categorie__modal__form-wrap">
-                    <label htmlFor="number">Ваш телефон</label>
-                    <input className='input-number' type="text" name='number' value={formData.tel} onChange={handleChange} placeholder='+998' />
-                </div> */}
-                <div className="categorie__modal-button">
-                    <button onClick={setTekshiruv} className='categorie__modal__form-btn'>Отправить</button>
-                </div>
-                </div>
-            </form>
+            <hr />
+            <div className="categorie__modal__form">
+              <div className="categorie__modal__form-wrap">
+                <label htmlFor="name">Ваше имя</label>
+                <input className='input-name' type="text" name='name' value={formData.name} onChange={handleChange} />
+              </div>
+              <div className="categorie__modal__form-wrap">
+                <label htmlFor="tel">Ваш телефон</label>
+                <input className='input-number' type="tel" name='tel' value={formData.tel} onChange={handleChange} placeholder='+998' />
+              </div>
+              <div className="categorie__modal-button">
+                <button onClick={setTekshiruv} className='categorie__modal__form-btn'>Отправить</button>
+              </div>
+            </div>
+          </form>
         </div>
+      </div>
+
     </div>
-    ))
-}
-</div>
   );
 };
 
